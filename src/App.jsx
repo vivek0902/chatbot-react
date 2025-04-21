4697ea3d9fe1bca846061acf11ad26eac9f421cf
Vivek Sharma <viveksharma090297@gmail.com>
2025-04-21 18:48:28 +0530

    initiate the project, add Routers and Protected routers for user dashbord, add pages

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import Dashboard from "./pages/Dashboard";
+function App() {
+  return (
+    <div>
+      <Routes>
+        {/*----------------------Public Pages / Unprotected------------------------------*/}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+
+        {/*------------------Protected User---------------------------*/}
+        <Route element={<PrivateRoute allowed={["user"]} />}>
+          <Route path="/user" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
