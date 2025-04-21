4f19755b12aaafc9da7288d452dc167b689ab015
Vivek Sharma <viveksharma090297@gmail.com>
2025-04-21 20:05:28 +0530

    add authContext api

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,17 @@
+import React, { Children, createContext, useState } from "react";
+export const AuthContext = createContext();
+
+export const AuthProvider = (props) => {
+  const [userRole, setUserRole] = useState(null);
+  const [credential, setCredential] = useState({
+    userId: "vivek",
+    password: "pass",
+  });
+  return (
+    <AuthContext.Provider
+      value={{ userRole, setUserRole, credential, setCredential }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
